c15304e53d2efdb581c448cba8750da8571d4ba4
dn200978lak <dn200978lak@gmail.com>
2015-03-12 12:10:10 +0200

    0.0.0

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php 
+//$DB_HOST = '127.0.0.1';
+$DB_HOST = '10.13.71.153';
+$DB_USER = 'root';
+$DB_PASS = '1111';
+$DB_NAME = 'angularcode_grid';
+$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+?>
